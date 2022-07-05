99ef12b416cc23d9dc152b831a4accc87d0e6ce0
KoalaAssasin <koalaassasinn@gmail.com>
2022-07-05 15:00:23 +1000

    sprites and better spawns - Micah
    -background sprites added
    -enemy spawns majorly improved
    -code cleaned so that additions of enemy types and change to spawn behaviour will be easy

--- Journey-of-the-Squid-King/Assets/Scripts/Enemy_scripts/enemy_spawning.cs
+++ Journey-of-the-Squid-King/Assets/Scripts/Enemy_scripts/enemy_spawning.cs
@@ -6,15 +6,30 @@ public class enemy_spawning : MonoBehaviour
 {
     public GameObject Enemy;
     float spawnTimer = 0.0f;
-    float spawnDelay = 9.0f;
+    public float spawnDelay = 9.0f;
+
+    int rnd;
 
     void Update()
     {
         if (spawnTimer > spawnDelay)
         {
-            Instantiate(Enemy, transform.position, transform.rotation);
-            spawnTimer = 0.0f;
-            //spawnDelay -= 0.1f;
+            rnd = Random.Range(1, 10);
+
+            if (rnd < 6)
+            {
+                Instantiate(Enemy, transform.position, transform.rotation);
+                spawnTimer = 0.0f;
+            }
+            else
+            {
+                spawnTimer = 1.3f;
+            }
+
+            if (spawnDelay > 3.0f)
+            {
+                spawnDelay -= 0.1f;
+            }
         }
 
         spawnTimer += Time.deltaTime;
